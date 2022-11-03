6c515ceb1f52a011d5350ae09a0da03c9ae04d97
Felix Cagatin <cagatingilbert@gmail.com>
2022-11-03 10:36:41 -0700

    Created GetEmployees and PrintEmployees methods

--- CatWorx.BadgeMaker/Program.cs
+++ CatWorx.BadgeMaker/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+namespace CatWorx.BadgeMaker
+{
+    class Program
+    {
+        // this will return a list of employees
+        static List<string> GetEmployees()
+        {
+            // Create and list to store employee names
+            List<string> employees = new List<string>();
+
+            while (true)
+            {
+                Console.WriteLine("Please enter a name: (Leave empty to exit): ");
+                string input = Console.ReadLine() ?? "";
+                if (input == "")
+                {
+                    break;
+                }
+                employees.Add(input);
+            }
+            return employees;
+        }
+        static void PrintEmployees(List<string> employees)
+        {
+            for (int i = 0; i < employees.Count; i++)
+            {
+                Console.WriteLine(employees[i]);
+            }
+        }
+        static void Main(string[] args)
+        {
+            // Create and list to store employee names
+            List<string> employees = GetEmployees();
+            PrintEmployees(employees);
+        }
+    }
+}
+
